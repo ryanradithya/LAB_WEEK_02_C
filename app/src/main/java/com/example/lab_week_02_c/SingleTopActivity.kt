40faf3776234b40050260edc9d880c3644ce7a0b
Ryan <ryanradithya2005@gmail.com>
2025-09-07 19:35:35 +0700

    Commit No. 4 – Standard activity and single top activity

--- app/src/main/java/com/example/lab_week_02_c/SingleTopActivity.kt
+++ app/src/main/java/com/example/lab_week_02_c/SingleTopActivity.kt
@@ -3,51 +3,43 @@ package com.example.lab_week_02_c
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SingleTopActivity : AppCompatActivity() {
     companion object{
         private const val DEBUG = "DEBUG"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_single_top)
         Log.d(DEBUG, "onCreate")
 
-        val buttonClickListener = View.OnClickListener { view ->
-            when (view.id) {
-                R.id.button_standard -> startActivity(
-                    Intent(
-                        this,
-                        StandardActivity::class.java
-                    )
+        findViewById<Button>(R.id.button_single_top).setOnClickListener{
+            startActivity(
+                Intent(
+                    this,
+                    SingleTopActivity::class.java
                 )
-                R.id.button_single_top -> startActivity(
-                    Intent(this,
-                        SingleTopActivity::class.java)
-                )
-            }
+            )
         }
+    }
 
-
-        findViewById<Button>(R.id.button_standard).setOnClickListener(buttonClickListener)
-
-        findViewById<Button>(R.id.button_single_top).setOnClickListener(buttonClickListener
-        )
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        Log.d(DEBUG, "onNewIntent")
     }
 }
 
-//class MainActivity : AppCompatActivity() {
+//class SingleTopActivity : AppCompatActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_single_top)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/lab_week_02_c/StandardActivity.kt
+++ app/src/main/java/com/example/lab_week_02_c/StandardActivity.kt
@@ -3,51 +3,42 @@ package com.example.lab_week_02_c
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+
+class StandardActivity : AppCompatActivity() {
     companion object{
         private const val DEBUG = "DEBUG"
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_standard)
         Log.d(DEBUG, "onCreate")
-
-        val buttonClickListener = View.OnClickListener { view ->
-            when (view.id) {
-                R.id.button_standard -> startActivity(
-                    Intent(
-                        this,
-                        StandardActivity::class.java
-                    )
+        findViewById<Button>(R.id.button_standard).setOnClickListener{
+            startActivity(
+                Intent(
+                    this,
+                    StandardActivity::class.java
                 )
-                R.id.button_single_top -> startActivity(
-                    Intent(this,
-                        SingleTopActivity::class.java)
-                )
-            }
+            )
         }
+    }
 
-
-        findViewById<Button>(R.id.button_standard).setOnClickListener(buttonClickListener)
-
-        findViewById<Button>(R.id.button_single_top).setOnClickListener(buttonClickListener
-        )
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        Log.d(DEBUG, "onNewIntent")
     }
 }
 
-//class MainActivity : AppCompatActivity() {
+//class StandardActivity : AppCompatActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_standard)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
